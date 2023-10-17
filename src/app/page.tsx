4241753a7f8f02cb191b0af15eda355929955ce5
Nico Bachner <git@nbac.me>
2023-10-18 01:41:02 +1100

    feat: Header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,17 @@
-const Page = () => <></>
+const Page = () => (
+  <main className="px-6">
+    <section className="mx-auto flex h-screen flex-col justify-center sm:max-w-xl md:max-w-3xl lg:max-w-5xl">
+      <p className="bg-gradient-to-r from-orange-500 to-amber-500 bg-clip-text text-center text-[13vw] font-extrabold leading-none tracking-tight text-transparent sm:text-7xl md:text-8xl lg:text-9xl">
+        Student.
+      </p>
+      <span className="bg-gradient-to-r from-blue-500 to-cyan-500 bg-clip-text text-center text-[13vw] font-extrabold leading-none tracking-tight text-transparent sm:text-left sm:text-7xl md:text-8xl lg:text-9xl">
+        Developer.
+      </span>
+      <span className="bg-gradient-to-r from-red-500 to-pink-500 bg-clip-text text-center text-[13vw] font-extrabold leading-none tracking-tight text-transparent sm:text-right sm:text-7xl md:text-8xl lg:text-9xl">
+        Entrepreneur.
+      </span>
+    </section>
+  </main>
+)
 
 export default Page
