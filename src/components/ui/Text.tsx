33ddfd9ae470310f489b6d49bc32b0a1394f0c9f
Nico Bachner <git@nbac.me>
2024-12-05 02:07:38 +1100

    feat: text component improvements

--- src/components/ui/Text.tsx
+++ src/components/ui/Text.tsx
@@ -1,8 +1,9 @@
 import { cn } from '@/utils/cn'
 
-type TextProps = React.ComponentProps<'p'> & {
-  as?: 'h1' | 'h2' | 'h3' | 'p' | 'span'
+type TextProps = Omit<React.ComponentProps<'p'>, 'color'> & {
+  as?: 'h1' | 'h2' | 'h3' | 'p'
   size?: 'sm' | 'md' | 'lg' | 'xl' | '2xl' | '3xl' | '4xl'
+  color?: 1 | 2 | 3
 }
 
 const getTextSize = (size: TextProps['size']) => {
@@ -24,28 +25,78 @@ const getTextSize = (size: TextProps['size']) => {
   }
 }
 
+const getTextColor = (color: TextProps['color']) => {
+  switch (color) {
+    case 1:
+      return 'text-primary-1'
+    case 2:
+      return 'text-primary-2'
+    case 3:
+      return 'text-primary-3'
+  }
+}
+
 /**
  * Text
  *
  * @param as semantic html tag to render
  * @param size font size
+ * @param color text color - 1, 2, 3 \
+ * 1 - Headings & bold text \
+ * 2 - Regular text \
+ * 3 - Subdued text (e.g. metadata)
  */
 export const Text: React.FC<TextProps> = ({
   as = 'p',
   size,
+  color,
   className,
   ...props
 }) => {
   switch (as) {
     case 'h1':
-      return <h1 className={cn(getTextSize(size), className)} {...props} />
+      return (
+        <h1
+          className={cn(
+            getTextSize(size) ?? getTextSize('4xl'),
+            getTextColor(color) ?? getTextColor(3),
+            className,
+          )}
+          {...props}
+        />
+      )
     case 'h2':
-      return <h2 className={cn(getTextSize(size), className)} {...props} />
+      return (
+        <h2
+          className={cn(
+            getTextSize(size) ?? getTextSize('2xl'),
+            getTextColor(color) ?? getTextColor(3),
+            className,
+          )}
+          {...props}
+        />
+      )
     case 'h3':
-      return <h3 className={cn(getTextSize(size), className)} {...props} />
+      return (
+        <h3
+          className={cn(
+            getTextSize(size) ?? getTextSize('lg'),
+            getTextColor(color) ?? getTextColor(3),
+            className,
+          )}
+          {...props}
+        />
+      )
     case 'p':
-      return <p className={cn(getTextSize(size), className)} {...props} />
-    case 'span':
-      return <span className={cn(getTextSize(size), className)} {...props} />
+      return (
+        <p
+          className={cn(
+            getTextSize(size) ?? getTextSize('md'),
+            getTextColor(color) ?? getTextColor(2),
+            className,
+          )}
+          {...props}
+        />
+      )
   }
 }
