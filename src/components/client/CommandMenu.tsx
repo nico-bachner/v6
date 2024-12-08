2cfa107f1a77af38b4ac1a9370e5576e3fe68392
Nico Bachner <git@nbac.me>
2024-12-08 19:20:29 +1100

    fix: adjust cmd button size

--- src/components/client/CommandMenu.tsx
+++ src/components/client/CommandMenu.tsx
@@ -272,9 +272,9 @@ export const CommandMenu = () => {
             setOpen(!open)
           }}
           tabIndex={open ? -1 : 1}
-          className="cursor-pointer rounded-full bg-highlight-2 p-2 transition hover:scale-110 hover:bg-highlight-3 focus:scale-110 focus:bg-highlight-3 focus:outline-none sm:p-3 lg:p-4"
+          className="cursor-pointer rounded-full bg-highlight-2 p-3 transition hover:scale-110 hover:bg-highlight-3 focus:scale-110 focus:bg-highlight-3 focus:outline-none"
         >
-          <CommandIcon className="size-6 stroke-primary-3 sm:size-8 lg:size-10" />
+          <CommandIcon className="size-8 stroke-primary-3 sm:size-9 lg:size-10" />
         </button>
       </nav>
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -71,6 +71,7 @@ const config: Config = {
       6: '1.5rem',
       7: '1.75rem',
       8: '2rem',
+      9: '2.25rem',
       10: '2.5rem',
       12: '3rem',
       16: '4rem',
