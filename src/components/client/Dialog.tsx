7c1a34e29ebd4e9eb98ea02c32747d28cc9a8ff5
Nico Bachner <git@nbac.me>
2024-12-08 14:04:33 +1100

    fix: fix mobile viewport shift 2

--- src/components/client/Dialog.tsx
+++ src/components/client/Dialog.tsx
@@ -1,25 +1,54 @@
 'use client'
 
 import { Content, Overlay, Portal, Root } from '@radix-ui/react-dialog'
+import { useLayoutEffect, useState } from 'react'
 
 import { cn } from '@/utils/cn'
 
 export const Dialog: React.FC<React.ComponentProps<typeof Root>> = ({
   children,
   ...props
-}) => (
-  <Root {...props}>
-    <Portal>
-      <Overlay className={cn('backdrop-blur', 'fixed inset-0')} />
+}) => {
+  const [viewportDimensions, setViewportDimensions] = useState({
+    width: 0,
+    height: 0,
+  })
 
-      <Content
-        className={cn(
-          'rounded-lg border bg-highlight-1 shadow-lg',
-          'fixed left-[50dvw] top-[50dvh] z-50 w-full max-w-xl translate-x-[-50%] translate-y-[-50%]',
-        )}
-      >
-        {children}
-      </Content>
-    </Portal>
-  </Root>
-)
+  useLayoutEffect(() => {
+    const updateViewportDimensions = () => {
+      setViewportDimensions({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      })
+    }
+
+    updateViewportDimensions()
+
+    window.addEventListener('resize', updateViewportDimensions)
+
+    return () => {
+      window.removeEventListener('resize', updateViewportDimensions)
+    }
+  }, [])
+
+  return (
+    <Root {...props}>
+      <Portal>
+        <Overlay className={cn('backdrop-blur', 'fixed inset-0')} />
+
+        <Content
+          style={{
+            top: viewportDimensions.height / 2,
+            left: viewportDimensions.width / 2,
+          }}
+          className={cn(
+            'rounded-lg border bg-highlight-1 shadow-lg',
+            'fixed z-50 w-full max-w-xl translate-x-[-50%] translate-y-[-50%]',
+          )}
+        >
+          {children}
+        </Content>
+      </Portal>
+    </Root>
+  )
+}
