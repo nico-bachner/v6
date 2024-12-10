4d573b576468ef6637c296c26ba2d22d0fe934c0
Nico Bachner <git@nbac.me>
2024-12-10 20:14:03 +1100

    refactor: separate interactive card styles

--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -24,7 +24,8 @@ export const Card: React.FC<CardProps> = ({
   <Link
     href={href}
     className={cn(
-      'group relative flex flex-col gap-2 rounded-lg border bg-highlight-1 p-8 transition-colors hover:bg-highlight-2 focus:bg-highlight-2 focus:outline-none sm:gap-3 sm:p-10 lg:gap-4 lg:p-12',
+      'relative flex flex-col gap-2 rounded-lg border p-8 sm:gap-3 sm:p-10 lg:gap-4 lg:p-12',
+      'group bg-highlight-1 transition-colors hover:bg-highlight-2 focus:bg-highlight-2 focus:outline-none',
       className,
     )}
   >
@@ -37,7 +38,12 @@ export const Card: React.FC<CardProps> = ({
     </Text>
 
     {additionalInfo && (
-      <div className="absolute -right-3 -top-3 rounded border bg-highlight-1 px-4 py-2 transition-colors group-hover:bg-highlight-2 group-focus:bg-highlight-2">
+      <div
+        className={cn(
+          'absolute -right-3 -top-3 rounded border px-4 py-2',
+          'bg-highlight-1 transition-colors group-hover:bg-highlight-2 group-focus:bg-highlight-2',
+        )}
+      >
         <p className="text-sm font-light text-primary-1 sm:text-md lg:text-lg">
           {additionalInfo}
         </p>
